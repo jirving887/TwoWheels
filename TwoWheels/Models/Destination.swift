a28a2155e1b7a96349ad4f68fddbb2e5614dec49
jirving887 <jonathanirving10@gmail.com>
2025-04-23 18:01:02 -0400

    feat: provide initial value for Destination MapItem.

--- TwoWheels/Models/Destination.swift
+++ TwoWheels/Models/Destination.swift
@@ -15,7 +15,7 @@ class Destination : MapSelectable {
     @Transient
     var feature: MapFeature?
     @Transient
-    var mapItem: MKMapItem?
+    var mapItem: MKMapItem = .init(placemark: .init(coordinate: .init()))
     var title: String
     var address: String?
     var longitude: Double
