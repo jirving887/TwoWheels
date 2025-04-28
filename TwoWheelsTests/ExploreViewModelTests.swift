a4cb40c376d74d2b2c9061dfeb63463e7ae22976
jirving887 <jonathanirving10@gmail.com>
2025-04-28 15:12:49 -0400

    test: create test fixture

--- TwoWheelsTests/ExploreViewModelTests.swift
+++ TwoWheelsTests/ExploreViewModelTests.swift
@@ -10,16 +10,21 @@ import Testing
 @testable import TwoWheels
 
 struct ExploreViewModelTests {
+    let dataService: MockDataService
+    let sut: ExploreViewModel
+    let laneStadiumDestination: Destination
+    
+    init() {
+        dataService = MockDataService()
+        sut = ExploreViewModel(dataService: dataService)
+        let laneStadium = CLLocationCoordinate2D(latitude: 37.22001, longitude: -80.41804)
+        let laneStadiumItem = MKMapItem(placemark: MKPlacemark(coordinate: laneStadium))
+        laneStadiumDestination = Destination(laneStadiumItem)
+        laneStadiumDestination.title = "Lane Stadium"
+    }
     
     @Test
     func test_addDestination_shouldAddDestination() {
-        let dataService = MockDataService()
-        let sut = ExploreViewModel(dataService: dataService)
-        let laneStadium = CLLocationCoordinate2D(latitude: 37.22001, longitude: -80.41804)
-        let laneStadiumItem = MKMapItem(placemark: MKPlacemark(coordinate: laneStadium))
-        let laneStadiumDestination = Destination(laneStadiumItem)
-        laneStadiumDestination.title = "Lane Stadium"
-        
         sut.addDestination(laneStadiumDestination)
         
         #expect(dataService.destinationTitles == ["Lane Stadium"])
