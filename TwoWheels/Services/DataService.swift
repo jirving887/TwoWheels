8781d7180029cc745fae5e860366a1eb4d43be3c
jirving887 <jonathanirving10@gmail.com>
2025-04-28 12:49:41 -0400

    feat: creat DataService protocol

---
+++ TwoWheels/Services/DataService.swift
@@ -0,0 +1,16 @@
+//
+//  DataService.swift
+//  TwoWheels
+//
+//  Created by Jonathan Irving on 4/28/25.
+//
+
+import Foundation
+
+protocol DataService {
+    
+    func fetch() -> [Destination]
+    func add(_ data: Destination)
+    func remove(_ data: Destination)
+    func update(_ data: Destination)
+}
