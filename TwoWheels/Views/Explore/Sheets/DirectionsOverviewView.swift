8e39ec3debe78df1ee7b3a4d8a3950a49bb7a7c4
jirving887 <jonathanirving10@gmail.com>
2025-05-17 13:25:33 -0400

    Implement basic directions overview view

--- TwoWheels/Views/Explore/Sheets/DirectionsOverviewView.swift
+++ TwoWheels/Views/Explore/Sheets/DirectionsOverviewView.swift
@@ -5,11 +5,39 @@
 //  Created by Jonathan Irving on 5/17/25.
 //
 
+import MapKit
 import SwiftUI
 
 struct DirectionsOverviewView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Map()
+                .cornerRadius(20)
+            
+            HStack {
+                GroupBox {
+                    Text("Distance")
+                }
+                
+                GroupBox {
+                    Text("Travel Time")
+                }
+                
+                GroupBox {
+                    Text("ETA")
+                }
+            }
+            
+            Button {
+            } label: {
+                Text("Go")
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
+            .frame(maxWidth: .infinity)
+        }
+        .padding()
     }
 }
 
