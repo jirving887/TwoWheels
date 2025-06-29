34ab42101b5cd46e78566529beb8cedc16e7ce53
jirving887 <jonathanirving10@gmail.com>
2025-06-29 14:21:37 -0400

    create DirectionsServiceTests

---
+++ TwoWheelsTests/DirectionsServiceTests.swift
@@ -0,0 +1,16 @@
+//
+//  DirectionsServiceTests.swift
+//  TwoWheelsTests
+//
+//  Created by Jonathan Irving on 6/29/25.
+//
+
+import Testing
+
+struct DirectionsServiceTests {
+
+    @Test func zero() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
