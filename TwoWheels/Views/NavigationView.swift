762fb69c0e14242a3141e47a5c7e9b436a51b9d6
jirving887 <jonathanirving10@gmail.com>
2025-07-12 09:33:54 -0400

    Configure navigation map

--- TwoWheels/Views/NavigationView.swift
+++ TwoWheels/Views/NavigationView.swift
@@ -5,11 +5,16 @@
 //  Created by Jonathan Irving on 7/2/25.
 //
 
+import MapKit
 import SwiftUI
 
 struct NavigationView: View {
+    @State private var position = MapCameraPosition.userLocation(fallback: .automatic)
+    @State private var bounds = MapCameraBounds(minimumDistance: 500, maximumDistance: 1000)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(position: $position, bounds: bounds, interactionModes: [.zoom, .pitch]) {
+            UserAnnotation()
+        }
     }
 }
 
