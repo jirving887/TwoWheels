e068ee73b7ece777e7da6186a50620fc4189d620
jirving887 <jonathanirving10@gmail.com>
2025-07-12 11:43:01 -0400

    Update search results with nonempty search string

--- TwoWheels/ViewModels/SearchViewModel.swift
+++ TwoWheels/ViewModels/SearchViewModel.swift
@@ -6,13 +6,40 @@
 //
 
 import Foundation
+import MapKit
 
 @MainActor
 @Observable
 class SearchViewModel {
+    private let searchService: any MapSearching
+    private let region: MKCoordinateRegion
+
     var searchResults: [Destination] = []
+    var searchString = ""
+
+    init(region: MKCoordinateRegion, searchService: any MapSearching) {
+        self.region = region
+        self.searchService = searchService
+    }
 
     func search() async {
+        guard !searchString.isEmpty else {
+            searchResults = []
+            return
+        }
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchString
+        request.region = region
+        await search(request)
+    }
 
+    private func search(_ request: MKLocalSearch.Request) async {
+        do {
+            searchResults = try await searchService.search(with: request).compactMap {
+                Destination($0)
+            }
+        } catch {
+            searchResults = []
+        }
     }
 }
--- TwoWheelsTests/SearchViewModelTests.swift
+++ TwoWheelsTests/SearchViewModelTests.swift
@@ -7,23 +7,63 @@
 
 import Testing
 @testable import TwoWheels
+import MapKit
 
 @MainActor
 struct SearchViewModelTests {
+    let searchServiceSpy: SearchServiceSpy
+
+    init() {
+        searchServiceSpy = SearchServiceSpy()
+    }
 
     @Test
     func init_shouldHaveNoSearchResults() {
-        let sut = SearchViewModel()
+        let sut = makeSUT()
 
         #expect(sut.searchResults.isEmpty)
     }
 
     @Test
     func search_withEmptySting_shouldReturnNoResults() async {
-        let sut = SearchViewModel()
+        let sut = makeSUT()
 
         await sut.search()
-        
+
         #expect(sut.searchResults == [])
+    }
+
+    @Test
+    func search_withNonEmptyString_shouldUpdateSearchResults() async {
+        let destination1 = Destination(
+            latitude: 37.22001,
+            longitude: -80.41804,
+            title: "Lane Stadium 1"
+        )
+        let destination2 = Destination(
+            latitude: 37.22001,
+            longitude: -80.41804,
+            title: "Lane Stadium 2"
+        )
+        let expectedSearchResults = [destination1, destination2]
+        searchServiceSpy.expectedSearchResults = expectedSearchResults.map { result in
+            let placemark = MKPlacemark(coordinate: result.coordinate)
+            let item = MKMapItem(placemark: placemark)
+            item.name = result.title
+            return item
+        }
+        let sut = makeSUT()
+        sut.searchString = "Lane Stadium"
+
+        await sut.search()
+
+        #expect(sut.searchResults[0].title == "Lane Stadium 1")
+        #expect(sut.searchResults[1].title == "Lane Stadium 2")
+    }
+
+    // MARK: Helpers
+
+    func makeSUT(region: MKCoordinateRegion = .init()) -> SearchViewModel {
+        SearchViewModel(region: region, searchService: searchServiceSpy)
     }
 }
