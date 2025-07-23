63f5e11ad62de1beb8a418abbd62ab95000be2e8
jirving887 <jonathanirving10@gmail.com>
2025-07-22 22:34:14 -0400

    Detail navigation ui

--- TwoWheels/Views/NavigationView.swift
+++ TwoWheels/Views/NavigationView.swift
@@ -9,6 +9,8 @@ import MapKit
 import SwiftUI
 
 struct NavigationView: View {
+    @Environment(TwoWheelsViewModel.self) var twoWheelsViewModel
+
     @State private var position = MapCameraPosition.userLocation(fallback: .automatic)
     @State private var bounds = MapCameraBounds(minimumDistance: 500, maximumDistance: 1000)
     let route: MKRoute
@@ -16,18 +18,76 @@ struct NavigationView: View {
     var body: some View {
         Map(position: $position, bounds: bounds, interactionModes: [.zoom, .pitch]) {
             MapPolyline(route)
-                .stroke(.blue, style: StrokeStyle(
-                        lineWidth: 5,
-                        lineCap: .round,
-                        lineJoin: .round
-                    )
-                )
+                .stroke(.blue, style:
+                            StrokeStyle(
+                                lineWidth: 5,
+                                lineCap: .round,
+                                lineJoin: .round
+                            )
+                        )
 
-            UserAnnotation()
+            UserAnnotation {
+                ZStack {
+                    Circle()
+                        .fill(.blue)
+
+                    Image(systemName: "motorcycle.fill")
+                        .foregroundStyle(.white)
+                        .padding(10)
+                }
+            }
         }
+        .overlay(alignment: .bottom) {
+            Button {
+                twoWheelsViewModel.endNavigation()
+            } label: {
+                Text("End Navigation")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.red)
+            .frame(maxWidth: .infinity, maxHeight: 100)
+            .cornerRadius(50)
+            .padding()
+        }
+        .overlay(alignment: .top) {
+            ZStack {
+                Color(.gray)
+                    .opacity(0.3)
+
+                VStack(alignment: .leading) {
+                    HStack {
+                        ForEach(0..<3) { _ in
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 35)
+                                    .foregroundStyle(.gray.opacity(0.3))
+                                    .frame(maxHeight: 100)
+
+                                Text("info")
+                            }
+                        }
+                    }
+
+                    HStack {
+                        Image(systemName: "arrow.turn.up.left")
+                            .resizable()
+                            .scaledToFit()
+
+                        Text("Directions")
+                    }
+                    .frame(maxHeight: .infinity)
+                }
+                .padding()
+            }
+            .frame(maxHeight: 175)
+            .cornerRadius(50)
+            .padding(.horizontal)
+        }
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
 #Preview {
     NavigationView(route: .init())
+        .environment(TwoWheelsViewModel())
 }
