63dd6ce275a850cb4530b8035866cef1c5d845d6
jirving887 <jonathanirving10@gmail.com>
2025-08-17 15:41:17 -0400

    Refactor to add test initializer

--- TwoWheelsTests/ExploreViewModelTests.swift
+++ TwoWheelsTests/ExploreViewModelTests.swift
@@ -9,10 +9,18 @@ import Testing
 @testable import TwoWheels
 
 struct ExploreViewModelTests {
+    let locationManagerSpy: CLLocationManagerSpy
+    let sut: ExploreViewModel
+
+    init() {
+        locationManagerSpy = CLLocationManagerSpy()
+        sut = ExploreViewModel(locationManager: locationManagerSpy)
+    }
+
     @Test
     func init_shouldCreateLocationManager() {
-        let clLocationManagerSpy = CLLocationManagerSpy()
-        _ = ExploreViewModel(locationManager: clLocationManagerSpy)
+        #expect(locationManagerSpy.requestCallCount == 1)
+    }
 
         #expect(clLocationManagerSpy.requestCallCount == 1)
     }
